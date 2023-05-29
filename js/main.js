ee5891aafd1d9bfb10ef5197fd8872b6e38d909b
Olexander Derevianko <derevyankoolx@gmail.com>
2023-05-29 21:13:44 +0300

    finish work

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-$(function() {
+$(function () {
     $('.slider__inner').slick({
         dots: true,
         arrows: false
 
+    });
+
+    $('.works__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="../images/slider/slide-left.svg" alt=""></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="../images/slider/slide-right.svg"></button>'
     });
 });
